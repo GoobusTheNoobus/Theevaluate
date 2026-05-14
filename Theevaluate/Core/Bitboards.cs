d72c1c19cc402f132ac1e5ee1d3ff4fb297414e7
Goobus <huand2464@wrdsb.ca>
2026-05-14 11:42:46 -0400

    Added Init

--- Theevaluate/Core/Bitboards.cs
+++ Theevaluate/Core/Bitboards.cs
@@ -29,7 +29,7 @@ class Bitboards
         0x4040404040404040UL,
         0x8080808080808080UL,
     ];
-        
+
     private static readonly U64[] RankMasks = [
         0xFFUL,
         0xFF00UL,
@@ -42,13 +42,13 @@ class Bitboards
     ];
 
     private static readonly U64[] SquareMasks = [
-        1UL <<  0, 1UL <<  1, 1UL <<  2, 1UL <<  3, 1UL <<  4, 1UL <<  5, 1UL <<  6, 1UL <<  7, 
-        1UL <<  8, 1UL <<  9, 1UL << 10, 1UL << 11, 1UL << 12, 1UL << 13, 1UL << 14, 1UL << 15, 
-        1UL << 16, 1UL << 17, 1UL << 18, 1UL << 19, 1UL << 20, 1UL << 21, 1UL << 22, 1UL << 23, 
-        1UL << 24, 1UL << 25, 1UL << 26, 1UL << 27, 1UL << 28, 1UL << 29, 1UL << 30, 1UL << 31, 
-        1UL << 32, 1UL << 33, 1UL << 34, 1UL << 35, 1UL << 36, 1UL << 37, 1UL << 38, 1UL << 39, 
-        1UL << 40, 1UL << 41, 1UL << 42, 1UL << 43, 1UL << 44, 1UL << 45, 1UL << 46, 1UL << 47, 
-        1UL << 48, 1UL << 49, 1UL << 50, 1UL << 51, 1UL << 52, 1UL << 53, 1UL << 54, 1UL << 55, 
+        1UL <<  0, 1UL <<  1, 1UL <<  2, 1UL <<  3, 1UL <<  4, 1UL <<  5, 1UL <<  6, 1UL <<  7,
+        1UL <<  8, 1UL <<  9, 1UL << 10, 1UL << 11, 1UL << 12, 1UL << 13, 1UL << 14, 1UL << 15,
+        1UL << 16, 1UL << 17, 1UL << 18, 1UL << 19, 1UL << 20, 1UL << 21, 1UL << 22, 1UL << 23,
+        1UL << 24, 1UL << 25, 1UL << 26, 1UL << 27, 1UL << 28, 1UL << 29, 1UL << 30, 1UL << 31,
+        1UL << 32, 1UL << 33, 1UL << 34, 1UL << 35, 1UL << 36, 1UL << 37, 1UL << 38, 1UL << 39,
+        1UL << 40, 1UL << 41, 1UL << 42, 1UL << 43, 1UL << 44, 1UL << 45, 1UL << 46, 1UL << 47,
+        1UL << 48, 1UL << 49, 1UL << 50, 1UL << 51, 1UL << 52, 1UL << 53, 1UL << 54, 1UL << 55,
         1UL << 56, 1UL << 57, 1UL << 58, 1UL << 59, 1UL << 60, 1UL << 61, 1UL << 62, 1UL << 63,
     ];
 
@@ -279,7 +279,7 @@ class Bitboards
                 mask |= SquareMasks[(int)newsquare];
 
                 if ((blockers & SquareMasks[(int)newsquare]) != 0) break;
-            }   
+            }
         }
 
         return mask;
@@ -306,7 +306,7 @@ class Bitboards
                 mask |= SquareMasks[(int)newsquare];
 
                 if ((blockers & SquareMasks[(int)newsquare]) != 0) break;
-            }   
+            }
         }
 
         return mask;
@@ -314,34 +314,34 @@ class Bitboards
 
     private static int HashBishop(Square square, U64 blockers)
     {
-    #if BMI2
+#if BMI2
         return (int)Bmi2.X64.ParallelBitExtract(
             blockers,
             BishopMasks[(int)square]
         );
-    #else
+#else
         return (int)(
             ((blockers & BishopMasks[(int)square]) *
             BishopMagics[(int)square])
             >> (64 - BishopRelevancies[(int)square])
         );
-    #endif
+#endif
     }
 
     private static int HashRook(Square square, ulong blockers)
     {
-    #if BMI2
+#if BMI2
         return (int)Bmi2.X64.ParallelBitExtract(
             blockers,
             RookMasks[(int)square]
         );
-    #else
+#else
         return (int)(
             ((blockers & RookMasks[(int)square]) *
             RookMagics[(int)square])
             >> (64 - RookRelevancies[(int)square])
         );
-    #endif
+#endif
     }
 
     private static U64 GenBlocker(int i, U64 mask)
@@ -362,7 +362,21 @@ class Bitboards
 
     public static void Initialize()
     {
-        #warning hasn't been implemented yet
+
+        for (Square square = Square.A1; square <= Square.H8; ++square)
+        {
+            for (int i = 0; i < (1 << BishopRelevancies[(int)square]); ++i)
+            {
+                U64 blockers = GenBlocker(i, BishopMasks[(int)square]);
+                BishopAttacks[BishopOffsets[(int)square] + HashBishop(square, blockers)] = RaycastBishop(square, blockers);
+            }
+
+            for (int i = 0; i < (1 << RookRelevancies[(int)square]); ++i)
+            {
+                U64 blockers = GenBlocker(i, RookMasks[(int)square]);
+                RookAttacks[BishopOffsets[(int)square] + HashRook(square, blockers)] = RaycastRook(square, blockers);
+            }
+        }
     }
 
 }
